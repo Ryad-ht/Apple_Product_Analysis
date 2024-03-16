baf37a44009da045f44d37e06b6a1b13179d37f1
Ryad-ht <hadjtahar.ryad@gmail.com>
2024-03-16 20:20:04 +0100

    .

--- main.py
+++ main.py
@@ -53,3 +53,15 @@ sns.boxplot(x='Ram', y='Sale Price', data=apple_products)
 plt.title('Sale Price by RAM Configuration')
 plt.show()
 
+##### Temporal Trends ###################################################
+
+# Convert Sale_Date to datetime
+apple_products['Sale_Date'] = pd.to_datetime(apple_products['Sale_Date'])
+# Group data by month or year depending on the data
+apple_products['Year'] = apple_products['Sale_Date'].dt.year
+yearly_trends = apple_products.groupby('Year').agg({'Sale Price': 'mean', 'Number Of Ratings': 'sum'})
+# Plot the trends over time
+yearly_trends['Sale Price'].plot(title='Average Sale Price Over Time', ylabel='Average Sale Price')
+plt.show()
+yearly_trends['Number Of Ratings'].plot(title='Total Number of Ratings Over Time', ylabel='Number Of Ratings')
+plt.show()
